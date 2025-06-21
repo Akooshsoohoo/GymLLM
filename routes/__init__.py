430623607dfbf283e6f67cf465e7b6016f8cca03
Akooshsoohoo <1sahakash1@gmail.com>
2025-06-21 18:38:17 -0400

    added blueprints to init.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,12 @@
-from .auth_routes import auth_bp
 from .search_routes import search_bp
 from .review_routes import review_bp
-from .home_routes import home_bp
 from .confirm_routes import confirm_bp
+from .home_routes import home_bp
+from .auth_routes import auth_bp
 
-all_blueprints = [auth_bp, search_bp, review_bp, home_bp, confirm_bp]
+def register_blueprints(app):
+    app.register_blueprint(search_bp)
+    app.register_blueprint(review_bp)
+    app.register_blueprint(confirm_bp)
+    app.register_blueprint(home_bp)
+    app.register_blueprint(auth_bp)
